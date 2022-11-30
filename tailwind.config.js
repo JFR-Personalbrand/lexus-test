2666828b3f7a1bb2541262d74046972fe7c02356
jorge rondon <jfrrj19@gmail.com>
2022-11-30 16:56:48 -0500

    enviroment setted

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+
+/**  @type {import('tailwindcss').Config} **/
+
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
@@ -8,5 +12,43 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: 
+  [
+   plugin(function ({ addComponents }) {
+      addComponents({
+        '.title-6': {
+          fontSize: '0.75rem',
+          fontStyle: 'normal',
+          fontWeight: '300',
+          lineHeight: '1.875rem',
+          letterSpacing: '.12rem',
+        },
+
+        '.display-2': {
+          fontSize: '2.25rem',
+          fontWeight: '300',
+          fontStyle: 'normal',
+          lineHeight: '43px',
+          letterSpacing: '-0.02em',
+        },
+
+        '.text-body': {
+          fontSize: '1rem',
+          fontWeight: '400',
+          fontStyle: 'normal',
+          lineHeight: '25.6px',
+        },
+
+        '.text-button': {
+          fontSize: '1.125rem',
+          fontWeight: '700',
+          fontStyle: 'Normal',
+          lineHeight: '28.8px',
+          letterSpacing: '-0.02em',
+        },
+
+        
+      })
+    })
+  ],
 }
