8387863a047d8fe1565a350d705da8a866ed7e23
jorge rondon <jfrrj19@gmail.com>
2022-12-01 15:37:16 -0500

    sticky buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Features from '../components/Features'
+import IconBolt from '../components/icons/IconBolt'
 import Lateralbar from '../components/Lateralbar'
 import Navbar from '../components/Navbar'
 import ServingDubling from '../components/ServingDubling'
@@ -21,7 +22,7 @@ const Home: NextPage = () => {
       <Navbar /> 
       <main className="w-full container mx-auto px-4 lg:px-1">
         <div className=" md:px-6 lg:px-8 pt-40 ">
-          <div className="flex flex-col md:flex-row justify-between items-start main-content">
+          <div className="flex flex-col md:flex-row justify-between items-start main-content pb-8">
             
             <div className="content md:pr-7 md:w-3/4">
               <div className="heading">
@@ -39,6 +40,12 @@ const Home: NextPage = () => {
 
               <TestDriveCTA />
 
+              <div className="my-6 text-xs text-dark-normal">
+                <small>*All figures are EPA estimates. Actual mileage may vary.</small>
+              </div>
+
+              
+
             </div>
             
             <Lateralbar />
@@ -49,7 +56,32 @@ const Home: NextPage = () => {
         </div>
         
       
-       
+        <div className="bottombuttons fixed bottom-0 right-0 w-full">
+              <div className="flex justify-between items-end">
+                  <div className="mx-10">
+                    <div className="">
+                      <button className="bg-secondary w-14 h-14 hover:-ml-1 hover:h-16 hover:w-16 flex justify-center items-center text-white text-lg shadow-md rounded-full mb-1 md:mb-3 ease-in duration-300">
+                        <IconBolt className='m-auto' /> 
+                      </button>
+                    </div>
+                  </div>
+                  
+                  <div className="flex items-end flex-col-reverse md:flex-row px-6">
+                    <div className="w-[154px]">
+                      <button className="bg-dark hover:py-[14px] hover:px-[22px] hover:ml-[-6px] text-white text-lg px-4 py-3 rounded-full mb-1 md:mb-3 ease-in duration-300 shadow-md">VALUE TRADE</button>
+                    </div>
+
+                    <div className="w-[87px]">
+                      <button className="bg-secondary hover:py-[14px] hover:px-[22px] hover:ml-[-6px] text-white text-lg px-4 py-3 rounded-full mb-1 md:mb-3 ease-in duration-300 shadow-md">CHAT</button>
+                    </div>
+                    
+                    <div className="w-[87px]">
+                      <button className="bg-dark hover:py-[14px] hover:px-[22px] hover:ml-[-6px] text-white text-lg px-4 py-3 rounded-full mb-1 md:mb-3 ease-in duration-300 shadow-md">TEXT</button>
+                    </div>
+                  
+                  </div>  
+              </div>  
+        </div>
         
         
 
